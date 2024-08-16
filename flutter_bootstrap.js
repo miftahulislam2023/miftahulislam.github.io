1fca74f3f23906e87243383b8e83647f6365b266
Miftahul Islam <138649177+miftahulislam2023@users.noreply.github.com>
2024-08-16 23:16:29 +0600

    updated view

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2445462245"
+    serviceWorkerVersion: "1669757782"
   }
 });
